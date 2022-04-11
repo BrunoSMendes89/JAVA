33ea4c2e762b97cbe44281b44f2d02e71d8f6f2f
Bruno Mendes <brunosmendes89@gmail.com>
2022-04-11 11:46:37 -0300

    Array

---
+++ src/ExemploArray.java
@@ -0,0 +1,12 @@
+
+public class ExemploArray {
+
+	public static void main(String[] args) {
+
+		String[] nome = { "Teste1", "Pessoa 2", "Pessoa 3" };
+
+		for (String n : nome) { //foreach
+			System.out.println(n);
+		}
+	}
+}
